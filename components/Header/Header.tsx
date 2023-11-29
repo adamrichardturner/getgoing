e634934fc6d38e2fba2b3ed202e787056459f53c
Adam Turner <hello@adamrichardturner.dev>
2023-11-29 20:52:07 +0000

    Upating folder structure

---
+++ components/Header/Header.tsx
@@ -0,0 +1,4 @@
+import SearchComponent from './SearchComponent/SearchComponent'
+import NotificationComponent from './NotificationComponent/NotificationComponent'
+import ProfileComponent from './ProfileComponent/ProfileComponent'
+export default function Header() {}
