0b2482fe084a616770c8be5eb76c1942d0d1d9c8
Adam Turner <hello@adamrichardturner.dev>
2023-11-30 02:09:02 +0000

    Updated todoId route

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -2,15 +2,7 @@ export default function TodoDetail({ params }: { params: { todoId: string } }) {
   let { todoId } = params
   return (
     <div className="bg-very-light-gray dark:bg-very-dark-blue-bg min-h-screen">
-      <h2>ToDoId: {todoId}</h2>
-      {todo ? (
-        <div>
-          <p>{todo.content}</p> {/* Replace with the appropriate property */}
-          {/* Render other details of the 'todo' here */}
-        </div>
-      ) : (
-        <p>Todo not found</p>
-      )}
+      {todoId}
     </div>
   )
 }
