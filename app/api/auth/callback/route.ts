772229c5815379f5751cec811d5039fb644ab0e7
Adam Turner <hello@adamrichardturner.dev>
2023-12-17 11:15:29 +0000

    Modified route for auth ccallback

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -12,5 +12,5 @@ export async function GET(req: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code)
   }
 
-  return NextResponse.redirect(new URL('/account', req.url))
+  return NextResponse.redirect(new URL('/', req.url))
 }
